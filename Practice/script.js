371c1710f5013d6346cf901564e3f81395e91272
alaminhossainrifat <rif420aat@gmail.com>
2024-10-14 22:21:50 +0600

    Class 1

---
+++ Practice/script.js
@@ -0,0 +1,8 @@
+const number1 = Number(prompt('Enter the first number: '));
+const number2 = Number(prompt('Enter the second number: '));
+
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+console.log(`${number1} - ${number2} = ${number1 - number2}`);
+console.log(`${number1} * ${number2} = ${number1 * number2}`);
+console.log(`${number1} / ${number2} = ${number1 / number2}`);
+console.log(`${number1} % ${number2} = ${number1 % number2}`);
